f34161711c0684391f6f7744a6ecdb9bdb254537
Sailu M <sailu123@gmail.com>
2026-09-14 21:11:23 +0530

    actions

--- mongodb.sh
+++ mongodb.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
-LOGS_FILE="/var/log/shell-script/$0.log"
+LOGS_FILE="/var/log/shell-script/$(basename "$0").log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
