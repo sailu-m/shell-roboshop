fc47959395fd4cea02f1c2587c48ae76b98cd040
Sailu M <sailu123@gmail.com>
2026-09-25 20:58:39 +0530

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -50,6 +50,9 @@ VALIDATE $? "Downloading catalogue code"
 cd /app
 VALIDATE $? "Moving to app directory"
 
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
 unzip /tmp/catalogue.zip
 VALIDATE $? "Unzip catalogue code"
 
