a1e793b85b6f00ac4a186dd40e19b0972c06369c
Sailu M <sailu123@gmail.com>
2026-09-25 20:18:40 +0530

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
-LOGS_FILE="$LOGS_FOLDER/$0.log"
+LOGS_FILE="$LOGS_FOLDER/$(basename "$0").log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -13,7 +13,7 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 
-mkdir -p $LOGS_FOLDER
+mkdir -p "$LOGS_FOLDER"
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
