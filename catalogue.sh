7536a7ba9862e5e098b35036d8d30f95aa0f6660
Sailu M <sailu123@gmail.com>
2026-09-25 20:53:02 +0530

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -46,3 +46,21 @@ VALIDATE $? "Creating app directory"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOGS_FILE
 VALIDATE $? "Downloading catalogue code"
+
+cd /app
+VALIDATE $? "Moving to app directory"
+
+unzip /tmp/catalogue.zip
+VALIDATE $? "Unzip catalogue code"
+
+npm install
+VALIDATE $? "Installing dependencies"
+
+cp catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Created systemctl service"
+
+systemctl daemon-reload
+systemctl enable catalogue 
+systemctl start catalogue
+VALIDATE $? "Starting and enabling Catalogue"
+
