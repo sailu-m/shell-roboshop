c588f433b2cbc15fd57cf955a9291296c4c67433
Sailu M <sailu123@gmail.com>
2026-09-25 21:07:43 +0530

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -8,6 +8,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.sailu-devops.online
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
@@ -68,3 +69,8 @@ systemctl enable catalogue &>>$LOGS_FILE
 systemctl start catalogue
 VALIDATE $? "Starting and enabling Catalogue"
 
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y
+
+mongosh --host localhost:27017 --quiet --eval 'db.getMongo().getDBNames().indexOf("mydb")'
+mongosh --host $MONGODB_HOST </app/db/master-data.js
